ee6e2c755717e44c4030aa1558d864bb32b6f33c
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-06-12 12:24:27 +0300

    added year selector

--- static/js/main.js
+++ static/js/main.js
@@ -6,12 +6,15 @@ let calculateDayIndex = (day, month, year) => {
     let yearTemp1 = yy.toString();  // takes the year value and turns it to a string
     let yearTemp2 = yearTemp1[0] + yearTemp1[1]; // takes the initial first two digits
 
-    let cc = parseInt(yearTemp2); // Converts temporary value into an integer
+    let cc = parseInt(yearTemp2); // Converts temporary value into an integer and saves the century
 
-    // let yearTemp1 =
+    let yearTemp3 = yearTemp1[2] + yearTemp1[3]; // takes the last two digits
+
+    yy = parseInt(yearTemp3);
     
 
     console.log(cc);
+    console.log(yy);
 }
 
 calculateDayIndex(14, 10, 1994);
